ce6611de4112e4773ec58009147ce8726980ede9
justintime1888 <capitamjparra12@gmail.com>
2025-10-23 21:02:29 -0400

    Create New File JustinMParra for introduction of Justin

---
+++ introductions/JustinMParra.py
@@ -0,0 +1,15 @@
+"""
+Feel free to go off of this template to introduce yourself.
+Be creative! You can add more functions or whatever to your hearts content!
+"""
+
+def introduce():
+    print("Hello! My name is Justin. I'm excited to be part of the micromouse community!")
+    print("I love robotics and programming, and I'm looking forward to learning and collaborating with all of you.")
+    print("In my free time, I enjoy hiking, playing video games, and experimenting with new tech projects.")
+    print("Can't wait to see what we can achieve together!")
+    print("Feel free to reach out to me anytime!")
+    print("Let's build something amazing!")
+    print("Go micromouse!")
+if __name__ == "__main__":
+    introduce()
